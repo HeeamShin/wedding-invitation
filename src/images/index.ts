cefb2b770df21cc499b29efe751d8b8272018e01
HeeamShin <ska900222@gmail.com>
2025-12-28 20:24:18 +0900

    test commit

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,26 @@
-import coverImage from "./cover.png"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import coverImage from "./IMG_0754.jpg"
+import image1 from "./IMG_0046.jpg"
+import image2 from "./IMG_0102.jpg"
+import image3 from "./IMG_0136.jpg"
+import image4 from "./IMG_0177.jpg"
+import image5 from "./IMG_0237.jpg"
+import image6 from "./IMG_0270.jpg"
+import image7 from "./IMG_0321.jpg"
+import image8 from "./IMG_0331.jpg"
+import image9 from "./IMG_0403.jpg"
+import image10 from "./IMG_0426.jpg"
+import image11 from "./IMG_0460.jpg"
+import image12 from "./IMG_0492.jpg"
+import image13 from "./IMG_0521.jpg"
+import image14 from "./IMG_0531.jpg"
+import image15 from "./IMG_0584.jpg"
+import image16 from "./IMG_0685.jpg"
+import image17 from "./IMG_0696.jpg"
+import image18 from "./IMG_0767.jpg"
+import image19 from "./IMG_0771.jpg"
+import image20 from "./QWE_5212.jpg"
+import image21 from "./QWE_5339.jpg"
+
 
 export const COVER_IMAGE = coverImage
 
@@ -27,4 +37,13 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
 ]
