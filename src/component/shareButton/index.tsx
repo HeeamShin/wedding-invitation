1f163ce723e82ae1e1f5702cb1c249a52881a8af
HeeamShin <ska900222@gmail.com>
2025-12-28 22:48:06 +0900

    change thumbnail

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -37,7 +37,7 @@ export const ShareButton = () => {
                 "//" +
                 window.location.host +
                 baseUrl +
-                "/preview_image.png",
+                "/IMG_0531.jpg",
               link: {
                 mobileWebUrl:
                   window.location.protocol +
